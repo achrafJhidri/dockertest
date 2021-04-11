812c1deac182ebfbc47d3169ce27c3d253a6aa9d
achraf jhidri <achrafjh@gmail.com>
2021-04-11 18:28:02 +0200

    connect two containers

--- myBackend/src/app.js
+++ myBackend/src/app.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+let count ;
+
+MongoClient.connect("mongodb://db",{useUnifiedTopology:true},(err,client) =>{
+    if (err)
+        console.log(err);
+    else
+        count = client.db("test").collection("data");
+})
+
+
+
 const app = express();
 
 app.get("*",(req,res) => { 
-    res.status(200).json("Hello   world !");
+    count.findOneAndUpdate({},{$inc :  {cnt : 1 }},{returnNewDocument:true}).then((doc) =>{
+        const cnt = doc.value.cnt;
+        res.status(200).json(cnt);
+    })
 })
 
 app.listen(80);
