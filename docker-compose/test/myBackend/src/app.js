86b929b212a6f4b100f1742ea5eb869cd0fc799d
achraf jhidri <achrafjh@gmail.com>
2021-04-19 02:05:49 +0200

    minimal setup for work

--- docker-compose/test/myBackend/src/app.js
+++ docker-compose/test/myBackend/src/app.js
@@ -1,24 +1,25 @@
 const express = require("express");
-// const MongoClient = require("mongodb").MongoClient;
-// let count ;
+const MongoClient = require("mongodb").MongoClient;
+let count ;
 
-// MongoClient.connect("mongodb://db",{useUnifiedTopology:true},(err,client) =>{
-//     if (err)
-//         console.log(err);
-//     else
-//         count = client.db("test").collection("data");
-// })
+MongoClient.connect("mongodb://db",{useUnifiedTopology:true},(err,client) =>{
+    if (err)
+        console.log(err);
+    else
+        count = client.db("test").collection("count");
+})
 
 
 
 const app = express();
 
-app.get("*",(req,res) => { 
-    // count.findOneAndUpdate({},{$inc :  {cnt : 1 }},{returnNewDocument:true}).then((doc) =>{
-    //     const cnt = doc.value.cnt;
-    //     res.status(200).json(cnt);
-    // })
-    res.status(200).json("Hello br  other")
+app.get("*",(req,res) => {
+    count.findOneAndUpdate({},{$inc :  {count : 1 }},{returnNewDocument:true})
+    .then((doc) =>{
+        const cnt = doc.value.count;
+        res.status(200).json(cnt);
+    })
+
 })
 
 app.listen(80);
